38680766e7eff866c0a6349df128a9ae390c1efe
Joel Júnior <joel.j@escolar.ifrn.edu.br>
2023-06-03 18:20:43 -0300

    Separate code in functions for better readability

--- automacao_backups_foundry/main.py
+++ automacao_backups_foundry/main.py
@@ -1,10 +1,23 @@
+import argparse
 import os
 
 import paramiko
-from decouple import config
+from dotenv import load_dotenv
 
 
 def connect_to_server(key_path, hostname, port, username):
+    """
+    Establishes an SSH connection to the remote server using the provided credentials.
+
+    Args:
+        key_path (str): Path to the private key file.
+        hostname (str): Remote server hostname or IP address.
+        port (int): SSH port number.
+        username (str): SSH username.
+
+    Returns:
+        paramiko.SSHClient: Established SSH session.
+    """
     session = paramiko.SSHClient()
     session.load_system_host_keys()
     session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -14,39 +27,49 @@ def connect_to_server(key_path, hostname, port, username):
 
 
 def zip_folder(session, remote_directory, remote_zip_filename):
-    zip_command = ' '.join(['zip -r ', remote_zip_filename, remote_directory])
+    """
+    Zips a remote folder on the server.
+
+    Args:
+        session (paramiko.SSHClient): Established SSH session.
+        remote_directory (str): Path to the remote directory to be zipped.
+        remote_zip_filename (str): Name of the remote ZIP file to be created.
+    """
+    zip_command = f'zip -r {remote_zip_filename} {remote_directory}'
     _, stdout, _ = session.exec_command(zip_command)
     stdout.read()
 
 
 def download_zip_folder(
-    session, remote_zip_filename, local_directory, local_zip_filename
+    session,
+    remote_zip_filename,
+    save_download_directory,
+    local_zip_filename,
 ):
+    """
+    Downloads a ZIP file from the remote server.
+
+    Args:
+        session (paramiko.SSHClient): Established SSH session.
+        remote_zip_filename (str): Name of the remote ZIP file to be downloaded.
+        save_download_directory (str): Local directory to save the downloaded ZIP file.
+        local_zip_filename (str): Name of the ZIP file to be saved locally.
+    """
     sftp = session.open_sftp()
-    sftp.get(
-        remote_zip_filename, os.path.join(local_directory, local_zip_filename)
+    save_download_local_path = os.path.join(
+        save_download_directory, local_zip_filename
     )
+    sftp.get(remote_zip_filename, save_download_local_path)
     sftp.close()
 
 
 def delete_zip_and_close(session, remote_zip_filename):
-    session.exec_command('rm ' + remote_zip_filename)
+    """
+    Deletes the remote ZIP file and closes the SSH session.
+
+    Args:
+        session (paramiko.SSHClient): Established SSH session.
+        remote_zip_filename (str): Name of the remote ZIP file to be deleted.
+    """
+    session.exec_command(f'rm {remote_zip_filename}')
     session.close()
-
-
-if __name__ == '__main__':
-    key_path = config('KEY_PATH')
-    hostname = config('HOSTNAME')
-    port = config('PORT')
-    username = config('USERNAME')
-    remote_directory = config('REMOTE_DIRECTORY')
-    remote_zip_filename = config('REMOTE_ZIP_FILENAME')
-    local_directory = config('LOCAL_DIRECTORY')
-    local_zip_filename = config('LOCAL_ZIP_FILENAME')
-
-    session = connect_to_server(key_path, hostname, port, username)
-    zip_folder(session, remote_directory, remote_zip_filename)
-    download_zip_folder(
-        session, remote_zip_filename, local_directory, local_zip_filename
-    )
-    delete_zip_and_close(session, remote_zip_filename)
