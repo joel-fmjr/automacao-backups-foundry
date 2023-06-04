10d82fb2e440ae2bc0cd843dd08767d548c9a7df
Joel Júnior <joel.j@escolar.ifrn.edu.br>
2023-06-04 01:50:51 -0300

    Add log messages

--- foundry_backup_automation/main.py
+++ foundry_backup_automation/main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from datetime import date
+from logging import INFO, basicConfig, error, info
 
 import paramiko
 from dotenv import load_dotenv
@@ -19,11 +20,20 @@ def connect_to_server(key_path, server_ip, port, username):
     Returns:
         paramiko.SSHClient: Established SSH session.
     """
-    session = paramiko.SSHClient()
-    session.load_system_host_keys()
-    session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    private_key = paramiko.RSAKey.from_private_key_file(key_path)
-    session.connect(server_ip, port, username, pkey=private_key)
+    try:
+        session = paramiko.SSHClient()
+        session.load_system_host_keys()
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        private_key = paramiko.RSAKey.from_private_key_file(key_path)
+        session.connect(server_ip, port, username, pkey=private_key)
+    except paramiko.AuthenticationException:
+        error('Authentication failed. Please check the SSH credentials.')
+        return
+    except Exception as e:
+        error(f'An error occurred during connection: {e}')
+        return
+    
+    info(f'Connection established on {username}@{server_ip}.')
     return session
 
 
@@ -39,6 +49,7 @@ def zip_folder(session, remote_directory, remote_zip_filename):
     zip_command = f'zip -r {remote_zip_filename} {remote_directory}'
     _, stdout, _ = session.exec_command(zip_command)
     stdout.read()
+    info('Folder zipped.')
 
 
 def download_zip_folder(
@@ -62,6 +73,7 @@ def download_zip_folder(
     )
     sftp.get(remote_zip_filename, save_download_local_path)
     sftp.close()
+    info('ZIP file downloaded.')
 
 
 def delete_zip_and_close(session, remote_zip_filename):
@@ -74,26 +86,45 @@ def delete_zip_and_close(session, remote_zip_filename):
     """
     session.exec_command(f'rm {remote_zip_filename}')
     session.close()
+    info('ZIP file deleted from server.')
 
 
 def main(args):
     load_dotenv(dotenv_path=args.args_file)
     hoje = date.today()
+    
+    basicConfig(
+        level=INFO,
+        filename=os.getenv('LOG_FILE_PATH', 'foundry_backup_automation.log'),
+        filemode='a',
+        encoding='utf-8',
+        format='%(asctime)s - %(levelname)s - %(message)s',
+    )
 
-    key_path = os.getenv('KEY_PATH')
-    server_ip = os.getenv('SERVER_IP')
-    port = int(os.getenv('PORT'))
-    username = os.getenv('REMOTE_USERNAME')
-    remote_directory = os.getenv('REMOTE_DIRECTORY')
-    remote_zip_filename = os.getenv('REMOTE_ZIP_FILE_NAME')
-    remote_zip_filename = (
-        f'{remote_zip_filename}_{hoje.strftime("%d_%m_%Y")}.zip'
-    )
-    save_download_directory = os.getenv('SAVE_DOWNLOAD_DIRECTORY')
-    local_zip_filename = os.getenv('LOCAL_ZIP_FILE_NAME')
-    local_zip_filename = (
-        f'{local_zip_filename}_{hoje.strftime("%d_%m_%Y")}.zip'
-    )
+    try:
+        key_path = os.environ['KEY_PATH']
+        server_ip = os.environ['SERVER_IP']
+        port = int(os.environ['PORT'])
+        username = os.environ['REMOTE_USERNAME']
+        remote_directory = os.environ['REMOTE_DIRECTORY']
+        remote_zip_filename = os.environ['REMOTE_ZIP_FILE_NAME']
+        remote_zip_filename = (
+            f'{remote_zip_filename}_{hoje.strftime("%d_%m_%Y")}.zip'
+        )
+        save_download_directory = os.environ['SAVE_DOWNLOAD_DIRECTORY']
+        local_zip_filename = os.environ['LOCAL_ZIP_FILE_NAME']
+        local_zip_filename = (
+            f'{local_zip_filename}_{hoje.strftime("%d_%m_%Y")}.zip'
+        )
+    except KeyError as e:
+        error(f'Environment variable {e} not found!')
+        return
+    except ValueError as e:
+        error(f'Environment variable PORT must be an integer!')
+        return
+    except Exception as e:
+        error(f'Unexpected error: {e}')
+        return
 
     session = connect_to_server(key_path, server_ip, port, username)
     try:
@@ -104,6 +135,7 @@ def main(args):
             save_download_directory,
             local_zip_filename,
         )
+        print(f'Backup {remote_zip_filename} was successfully downloaded!')
     finally:
         delete_zip_and_close(session, remote_zip_filename)
 
